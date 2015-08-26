c2a658defd9c09c2e97aa0ab1f566513d55cb38a
Kincade Mccown <kincademccown@Alveyworld-14.local>
2015-08-26 13:06:54 -0600

    Pypet.py

---
+++ pypet.py
@@ -0,0 +1,31 @@
+print "Welcome to pypet"
+cat = {
+	
+	"name": "fluffy",
+	"hungry": True,
+	"weight": 9.5
+	"age": 5,
+	"photo": "(=^o.o^=)__"
+}
+dog = {
+	"name": "mouse",
+	"age": 6,
+	"weight": 1.5,
+	"hungry": False
+	"photo": "<:3 ) ~~~~~"
+}
+
+print "Hello" + cat['name']
+print cat['photo']
+
+def feed(pet):
+	if pet['hungry'] == True
+	pet['hungry'] = False
+	cat['weight'] = cat['weight'] + 1
+else:
+	print 'Thy pypet is not hungry'
+
+print cat
+feed(cat)
+print(cat)
+feed(cat)
