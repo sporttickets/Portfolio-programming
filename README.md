f2703c3fe7c85e012efb4e25e284fb37039a8b5c
Kincade Mccown <kincademccown@Alveyworld-14.local>
2015-09-14 10:23:22 -0600

    hi

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Portfolio-programming
 High School Programming Portfolio
 Hi
+print ‘ what do you want to name your pet?’
+cat name = input(‘Name your pet’)
+name == +cat name+ 
---
+++ deciding.py
@@ -0,0 +1,9 @@
+#learning about if statements 
+number = int(raw_input("enter your age\n"))
+if number < 15:
+	print "you still are in middle school"
+else:
+	print "you know everthing"
+if number < 5:
+	print "you are not in school"
+
